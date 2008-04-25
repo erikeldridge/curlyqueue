6ec4bd066da1c1e130db3fac9f7f2cba449a21de
erikeldridge <erikeldridge@8efcb38e-aa4b-0410-9e15-7fe0448eac4a>
2008-04-25 09:39:24 +0000

    cleaned up funky indentation

--- curlyqueue.h
+++ curlyqueue.h
@@ -47,22 +47,22 @@ typedef struct curlyqueue_node_value_t {
  *  a node in a doubly linked list (dll). 
  */
 struct curlyqueue_node {
-	void* 				value;
+	void*			value;
 	curlyqueue_node_t* 	prev;
 	curlyqueue_node_t* 	next;
 };
 
 /* BEGIN: method list */
 curlyqueue_t* 		curly_create_queue();
-void				curly_destroy_queue( curlyqueue_t* );
-void				curly_display_queue( curlyqueue_t* );
+void			curly_destroy_queue( curlyqueue_t* );
+void			curly_display_queue( curlyqueue_t* );
 curlyqueue_node_t* 	curly_create_node( void* value, curlyqueue_node_t *, curlyqueue_node_t * );
-void				curly_enqueue( curlyqueue_t* queue, void* value );
-void*				curly_dequeue( curlyqueue_t* queue );
+void			curly_enqueue( curlyqueue_t* queue, void* value );
+void*			curly_dequeue( curlyqueue_t* queue );
 curlyqueue_node_t*	curly_search( void* value );
-void				curly_insert_node( curlyqueue_t* queue, curlyqueue_node_t* node );
-void				curly_delete( curlyqueue_t*, curlyqueue_node_t* node );
-int					curly_queue_is_empty( curlyqueue_t* );
+void			curly_insert_node( curlyqueue_t* queue, curlyqueue_node_t* node );
+void			curly_delete( curlyqueue_t*, curlyqueue_node_t* node );
+int			curly_queue_is_empty( curlyqueue_t* );
 /* END: method list */
 
 #endif /*CURLYQUEUE_H_*/
