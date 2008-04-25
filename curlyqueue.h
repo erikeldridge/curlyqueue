d6c3aff0c93727f4de5a4381d482d5719869f6e1
erikeldridge <erikeldridge@8efcb38e-aa4b-0410-9e15-7fe0448eac4a>
2008-04-25 05:24:15 +0000

    made node value field a void pointer to increase flexibility of node

--- curlyqueue.h
+++ curlyqueue.h
@@ -24,16 +24,16 @@ typedef struct curlyqueue_node_value_t {
  *  a node in a doubly linked list (dll). 
  */
 struct curlyqueue_node {
-	curlyqueue_node_value_t value;
-	curlyqueue_node_t *prev;
-	curlyqueue_node_t *next;
+	void* 				value;
+	curlyqueue_node_t* 	prev;
+	curlyqueue_node_t* 	next;
 };
 
 /* BEGIN: method list */
 curlyqueue_t 		curly_create_queue();
 void				curly_destroy_queue( struct DLL * );
 void				curly_display_queue( struct DLL );
-curlyqueue_node_t* 	curly_create_node( curlyqueue_node_value_t, curlyqueue_node_t *, curlyqueue_node_t * );
+curlyqueue_node_t* 	curly_create_node( void* value, curlyqueue_node_t *, curlyqueue_node_t * );
 void				curly_enqueue( queue_t *, pcb_t );
 void				curly_dequeue( queue_t *, pcb_t * );
 void				curly_insert_node( queue_t*, pcb_t );
