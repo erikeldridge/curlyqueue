dca38c222120e7d21bdb06bf0671de085bb80bed
erikeldridge <erikeldridge@8efcb38e-aa4b-0410-9e15-7fe0448eac4a>
2008-04-25 20:09:02 +0000

    preparing for consolidation prior to release

---
+++ branches/release_candidate_1/curlyqueue.h
@@ -0,0 +1,69 @@
+#ifndef CURLYQUEUE_H_
+#define CURLYQUEUE_H_
+
+#ifndef NULL
+#define NULL	0
+#endif
+
+#ifdef DEBUG
+//int DEBUG = 0;
+#else
+//int DEBUG = 1;
+#endif
+
+/**
+ * A datatype to help implement exception handling
+ * @field	thrown	A quasi-Boolean value indicating
+ * 			if an exception was thrown (1) 
+ * 			or not (0).
+ * @field	type	A short string to describe the 
+ * 			type of exception thrown
+ */
+typedef struct exception {
+	int thrown;
+	char type[10];
+} except_t;
+
+typedef struct curlyqueue_node	curlyqueue_node_t;
+
+/**
+ * The head, tail, and count of a doubly linked list (dll).  
+ * Used for managing a dll 
+ */
+typedef struct curlyqueue_t {
+	curlyqueue_node_t* back;
+	curlyqueue_node_t* front;
+	int count;
+} curlyqueue_t;
+
+/**
+ * define this as you need
+ */
+typedef struct curlyqueue_node_value_t {
+	
+} curlyqueue_node_value_t;
+
+/**
+ *  a node in a doubly linked list (dll). 
+ */
+struct curlyqueue_node {
+	void*			value;
+	curlyqueue_node_t* 	prev;
+	curlyqueue_node_t* 	next;
+};
+
+/* BEGIN: method list */
+curlyqueue_t* 		curly_create_queue();
+void			curly_destroy_queue( curlyqueue_t* );
+curlyqueue_node_t* 	curly_create_node( void* value, curlyqueue_node_t *, curlyqueue_node_t * );
+
+void			curly_enqueue( curlyqueue_t* queue, void* value );
+void*			curly_dequeue( curlyqueue_t* queue, except_t* e );
+
+int			curly_queue_is_empty( curlyqueue_t* );
+
+void			curly_insert_before( curlyqueue_t* queue, curlyqueue_node_t* node, void* value );
+
+/* END: method list */
+
+#endif /*CURLYQUEUE_H_*/
