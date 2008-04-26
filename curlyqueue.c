f6ac2fa89105881b357447ef81f5933dcc359c97
erikeldridge <erikeldridge@8efcb38e-aa4b-0410-9e15-7fe0448eac4a>
2008-04-26 11:04:03 +0000

    tried impl opaque pointers, but removed them as they made development too trick, for the current stagey

--- curlyqueue.c
+++ curlyqueue.c
@@ -5,47 +5,15 @@
 #include "curlyqueue.h"
 
 /**
- * A datatype to help implement exception handling
- * @field	thrown	A quasi-Boolean value indicating
- * 			if an exception was thrown (1) 
- * 			or not (0).
- * @field	type	A short string to describe the 
- * 			type of exception thrown
- */
-struct except_t {
-	int thrown;
-	char type[10];
-};
-
-/**
- * The head, tail, and count of a doubly linked list (dll).  
- * Used for managing a dll 
- */
-struct curlyqueue_t {
-	curlyqueue_node back;
-	curlyqueue_node front;
-	int count;
-};
-
-/**
- *  a node in a doubly linked list (dll). 
- */
-struct curlyqueue_node_t {
-	void*			value;
-	curlyqueue_node 	prev;
-	curlyqueue_node 	next;
-};
-
-/**
  * Creates new list node
  * @pre		calling fn has determined prev and next pointers
  * @post	mem has been allocated for new node and
  * 		and node field values have been assigned
  * @note	calling fn must free mem!
  */
-curlyqueue_node curly_create_node( void* value, curlyqueue_node prev, curlyqueue_node next ){
+curlyqueue_node_t* curly_create_node( void* value, curlyqueue_node_t* prev, curlyqueue_node_t* next ){
 	
-	curlyqueue_node node = ( curlyqueue_node )malloc( sizeof( struct curlyqueue_node_t ) );
+	curlyqueue_node_t* node = ( curlyqueue_node_t* )malloc( sizeof( curlyqueue_node_t ) );
 	
 	node->value	= value;
 	node->prev	= prev;
@@ -61,9 +29,9 @@ curlyqueue_node curly_create_node( void* value, curlyqueue_node prev, curlyqueue
  * 		null (if pointers) or zero (if count)
  * @note	calling fn must free mem!
  */
-curlyqueue curly_create_queue(){
+curlyqueue_t* curly_create_queue(){
 	
-	curlyqueue queue = malloc( sizeof( curlyqueue_t ) );
+	curlyqueue_t* queue = malloc( sizeof( struct curlyqueue_t ) );
 	
 	queue->back		= NULL;
 	queue->front	= NULL;
@@ -80,17 +48,17 @@ curlyqueue curly_create_queue(){
  * @post	the list manager's pointers are set to null
  * @post	the list manager's count is decremented for each freed node
  */
-void curly_destroy_queue( curlyqueue queue ){
+void curly_destroy_queue( curlyqueue_t* queue ){
 	
 	/**
 	 the current node
 	 */
-	curlyqueue_node curr = queue->back;
+	curlyqueue_node_t* curr = queue->back;
 		
 	/**
 	 the node after current node
 	 */
-	curlyqueue_node next;
+	curlyqueue_node_t* next;
 	
 	/* while the current node isn't the end of the list*/
 	while( curr != NULL ){
@@ -118,12 +86,12 @@ void curly_destroy_queue( curlyqueue queue ){
  * @param	value	The value to push onto the q
  * @post	The queue's count is incremented
  */
-void curly_enqueue( curlyqueue queue, void* value ){
+void curly_enqueue( curlyqueue_t* queue, void* value ){
 	
-	curlyqueue_node prev = NULL;
-	curlyqueue_node next = queue->back;
+	curlyqueue_node_t* prev = NULL;
+	curlyqueue_node_t* next = queue->back;
 	
-	curlyqueue_node node = curly_create_node( value, prev, next );
+	curlyqueue_node_t* node = curly_create_node( value, prev, next );
 	
 	/* if this is the first node, the tail = the head*/
 	if( curly_queue_is_empty( queue ) ){
@@ -145,7 +113,7 @@ void curly_enqueue( curlyqueue queue, void* value ){
 /**
  * @except	throws "empty_q" exception if q is empty
  */
-void* curly_dequeue( curlyqueue queue, except_t* e ){
+void* curly_dequeue( curlyqueue_t* queue, except_t* e ){
 	
 	if( curly_queue_is_empty( queue ) ){
 		/* throw exception */
@@ -166,8 +134,8 @@ void* curly_dequeue( curlyqueue queue, except_t* e ){
 		queue->back	= NULL;
 
 	}else{
-		curlyqueue_node old_front	= queue->front;
-		curlyqueue_node new_front	= queue->front->prev;
+		curlyqueue_node_t* old_front	= queue->front;
+		curlyqueue_node_t* new_front	= queue->front->prev;
 
 		queue->front = new_front;
 		new_front->next = NULL;
@@ -189,7 +157,7 @@ void* curly_dequeue( curlyqueue queue, except_t* e ){
  * @return	1, if the queue is empty
  * @return 	0 if not empty
  */
-int curly_queue_is_empty( curlyqueue queue ){
+int curly_queue_is_empty( curlyqueue_t* queue ){
 	if( 0 == queue->count ){
 		return 1;
 	}else{
@@ -204,7 +172,7 @@ int curly_queue_is_empty( curlyqueue queue ){
  * 		value is now in the queue
  * @note	The calling function is responsible for freeing the memory allocated
  */
-void curly_insert_before( curlyqueue queue, curlyqueue_node marker, void* value ){
+void curly_insert_before( curlyqueue_t* queue, curlyqueue_node_t* marker, void* value ){
 	
 
 	if( curly_queue_is_empty( queue ) || ( marker == queue->back ) ){
@@ -213,10 +181,10 @@ void curly_insert_before( curlyqueue queue, curlyqueue_node marker, void* value 
 
 	} else {
 
-		curlyqueue_node_t *prev = marker->prev;
-		curlyqueue_node_t *next = marker;
+		curlyqueue_node_t* prev = marker->prev;
+		curlyqueue_node_t* next = marker;
 
-		curlyqueue_node node = curly_create_node( value, prev, marker );
+		curlyqueue_node_t* node = curly_create_node( value, prev, marker );
 
 		marker->prev->next	= node;
 		marker->prev	 	= node;
@@ -225,7 +193,7 @@ void curly_insert_before( curlyqueue queue, curlyqueue_node marker, void* value 
 	}
 }
 
-void curly_delete( curlyqueue queue, curlyqueue_node node ){
+void curly_delete( curlyqueue_t* queue, curlyqueue_node_t* node ){
 
 //	if( DEBUG ){ printf("  END: deletevalue \n"); }
 }
