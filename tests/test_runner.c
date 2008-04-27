45e25bc882e646019a583930aa88cfb6bff2ab0e
erikeldridge <erikeldridge@8efcb38e-aa4b-0410-9e15-7fe0448eac4a>
2008-04-27 08:15:21 +0000

    added insert and delete fn call

--- tests/test_runner.c
+++ tests/test_runner.c
@@ -1,11 +1,13 @@
 extern void test_suite_core();
 extern void test_suite_iterator();
+extern void test_suite_insert_and_delete();
 
 int main() {
 
 	test_suite_core();
 	test_suite_iterator();
-
+	test_suite_insert_and_delete();
+	
 	return 0;
 }
 
