da0c526dd51ab15fc9de11c5bdbcf052f99f3297
erikeldridge <erikeldridge@8efcb38e-aa4b-0410-9e15-7fe0448eac4a>
2008-04-28 08:17:43 +0000

    added macro defs for begin/end runner statements

--- tests/unit_test_util.h
+++ tests/unit_test_util.h
@@ -3,19 +3,25 @@
 
 #include "stdio.h"
 
-#define ADD_SUITE( suite_name )					\
+#define BEGIN_RUNNER()								\
+	printf("BEGIN: runner \n\n");	
+
+#define END_RUNNER()								\
+	printf("END: runner \n\n");	
+
+#define ADD_SUITE( suite_name )						\
 	printf("BEGIN: suite " #suite_name " \n\n");	\
-	suite_name();								\
+	suite_name();									\
 	printf("END: suite " #suite_name " \n\n");	
 
-#define ADD_TEST( test_name )					\
-	printf(" BEGIN: test " #test_name " \n");	\
-	test_name();								\
-	printf(" END: test " #test_name " \n\n");	\
+#define ADD_TEST( test_name )						\
+	printf(" BEGIN: test " #test_name " \n");		\
+	test_name();									\
+	printf(" END: test " #test_name " \n\n");		\
 
-#define ADD_CASE( case_name )					\
-	printf("  BEGIN: case " #case_name " \n");	\
-	case_name();								\
-	printf("  END: case " #case_name " \n\n");	\
+#define ADD_CASE( case_name )						\
+	printf("  BEGIN: case " #case_name " \n");		\
+	case_name();									\
+	printf("  END: case " #case_name " \n\n");		\
 	
 #endif /*UNIT_TEST_H_*/
