4a1176b0a89f7d2c93d8981a38a9b0b8ae8477e0
erikeldridge <erikeldridge@8efcb38e-aa4b-0410-9e15-7fe0448eac4a>
2008-04-28 10:01:29 +0000

    template for mainpage of doxygen documentation

---
+++ trunk/docs/mainpage.h
@@ -0,0 +1,9 @@
+#ifndef MAINPAGE_H_
+#define MAINPAGE_H_
+
+/**
+ * @mainpage
+ * 
+ * @section intro Introduction
+ * Curly queue is a simple queue implemented in C. It uses a doubly-linked list for the back-end storage and offers the following (public) functions:
+ */
