89117718d8b68718f3ecdf9dedcf645fb00656be
erikeldridge <erikeldridge@8efcb38e-aa4b-0410-9e15-7fe0448eac4a>
2008-04-28 08:00:27 +0000

    -removed commented-out code -used ADD_CASE macro in test def

--- tests/test_suite_insert_and_delete.c
+++ tests/test_suite_insert_and_delete.c
@@ -6,92 +6,6 @@
 #include "stdio.h"
 #include "string.h"
 
-/*
-void test_case_curly_insert_before(){
-	
-	curlyqueue_t* queue = curly_create_queue();
-
-	//assert( 0 == queue->count );	
-	//
-	// BEGIN: test w/ empty q
-
-	// BEGIN: test insert b4 back
-	int i = 1;
-	void* value = &i;
-	curly_insert_before( queue, queue->back, value );
-	
-	assert( 1 == *(int*)queue->back->value );
-	assert( 1 == *(int*)queue->front->value );
-
-	assert( NULL == queue->back->prev );
-	assert( NULL == queue->front->next );
-
-	//assert( 1 == queue->count );
-	//printf("%d \n", queue->count);
-	
-	// clear out q for next test
-	{except_t e;e.thrown=0;
-		curly_dequeue( queue, &e );
-		assert( 0 == e.thrown );// for sanity
-	}
-	assert( 0 == queue->count );// sanity check
-	// END: test insert b4 back
-
-	// BEGIN: test insert b4 front
-
-	int j = 2;
-	value = &j;
-	curly_insert_before( queue, queue->front, value );
-	
-	assert( 1 == queue->count );
-	assert( 2 == *(int*)queue->back->value );
-	assert( 2 == *(int*)queue->front->value );
-	assert( NULL == queue->back->prev );
-	assert( NULL == queue->front->next );
-	// END: test insert b4 front
-
-	// END: test w/ empty q
-
-	// BEGIN: test w/ non-empty q
-	int k = 3;
-	value = &k;
-	curly_enqueue( queue, value );
-
-	int l = 4;
-	value = &l;
-	curly_enqueue( queue, value );
-
-	int m = 5;
-	value = &m;
-	curly_enqueue( queue, value );
-
-	int n = 6;
-	value = &n;
-	curly_enqueue( queue, value );
-
-	assert( 5 == queue->count );
-
-	int o = 7;
-	value = &o;
-	curlyqueue_node_t *marker = queue->back->next->next;
-	curly_insert_before( queue, marker, value );
-
-	// assert front and back nodes were note altered
-	assert( 6 == *(int*)queue->back->value );
-	assert( 2 == *(int*)queue->front->value );
-
-	// assert new value (7) is correctly positioned
-	assert( 3 == *(int*)marker->next->value );
-	assert( 7 == *(int*)marker->prev->value );
-	assert( 5 == *(int*)marker->prev->prev->value );
-	
-	// END: test empty q
-	
-	
-	curly_destroy_queue( queue );
-}
-*/
-
 void test_curly_delete_value_at_iterator() {
 	
 	curlyqueue_t* queue = curly_create_queue();
@@ -363,31 +277,6 @@ void case_insert_value_after_iterator_with_single_elem_in_list(){
 	/* chk q back == old front */
 	assert( queue->back == old_front );
 	
-	/* BEGIN: walk list, printing values 
-	curlyqueue_iterator_jump_to_back( queue );
-	int val;
-	{except_t e;e.thrown=0;
-	
-		//print all values except front
-		while( curly_iterator_has_next( queue, &e ) ){
-				
-				val = *(int*)curly_get_value_at_iterator( queue, &e );	
-				
-				printf("val: %d \n", val );
-				
-				curlyqueue_iterator_step_forward( queue, &e );
-				
-				assert( 0 == e.thrown );
-		}
-		
-		//print front value
-		val = *(int*)curly_get_value_at_iterator( queue, &e );	
-		assert( 0 == e.thrown );
-		
-		printf("val: %d \n", val );
-	}
-	/* END: walk list, printing values */
-	
 	curly_destroy_queue( queue );
 }
 
@@ -463,9 +352,9 @@ void case_insert_value_after_non_front_iter_with_multi_elem_list(){
 }
 
 void test_curlyqueue_insert_value_after_iterator(){
-	case_insert_value_after_iterator_in_empty_list();
-	case_insert_value_after_iterator_with_single_elem_in_list();
-	case_insert_value_after_non_front_iter_with_multi_elem_list();
+	ADD_CASE( case_insert_value_after_iterator_in_empty_list )
+	ADD_CASE( case_insert_value_after_iterator_with_single_elem_in_list )
+	ADD_CASE( case_insert_value_after_non_front_iter_with_multi_elem_list );
 }
 /* END: testing insert_after_iter */
 
