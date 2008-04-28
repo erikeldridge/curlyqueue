ccf75760da9ee596940fa283fd5191318bb603bd
erikeldridge <erikeldridge@8efcb38e-aa4b-0410-9e15-7fe0448eac4a>
2008-04-28 10:12:42 +0000

    doxy -adjusted width of treeview col
    mainpage
    -added sample content

--- docs/mainpage.h
+++ docs/mainpage.h
@@ -6,4 +6,15 @@
  * 
  * @section intro Introduction
  * Curly queue is a simple queue implemented in C. It uses a doubly-linked list for the back-end storage and offers the following (public) functions:
+ * 	- create_queue()
+ * 		- to create a queue 
+ * 	- destroy_queue()
+ * 		- safely destroy the queue 
+ * 	- is_empty()
+ * 		- returns true if empty 
+ *	- enqueue()
+ * 		- to push onto the queue 
+ *	- dequeue()
+ * 		- to pop from the queue 
+ *	- with more on the way!* 
  */
